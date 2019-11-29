62d62fed11ef30a6e23fff3f05a62ebfe2a2f1ab
Ruggeri Barbosa <ruggeri.nascimento@hotmail.com.br>
2019-11-29 10:31:25 -0400

    Primeiro commit do novo modelo de integração

---
+++ EuDiretoService/ProjectInstaller.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EuDiretoService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            this.serviceProcessInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceProcessInstaller1_AfterInstall);
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.ServiceName = "EuDireto Sync";
+            this.serviceInstaller1.Description = "EuDireto Sync";
+            this.serviceInstaller1.Description = "Serviço de sincronização de dados ERP para o portal EuDireto";
+            this.serviceInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceInstaller1_AfterInstall);
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ EuDiretoService/Service1.cs
@@ -0,0 +1,246 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace EuDiretoService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            this.ServiceName = "EuDiretoSync";
+        }
+        Timer upProdutos = new Timer();
+        OracleConnection con;
+        DateTime ult_sinc_produtos = DateTime.MinValue;
+        DateTime timer = DateTime.MinValue;
+
+        protected override void OnStart(string[] args)
+        {
+            try { 
+               con = new OracleConnection(Properties.Settings.Default.stringConection);
+            }catch(Exception ex)
+            {
+                erroLogGeneration(ex.ToString());
+            }
+            upProdutos.Elapsed += new ElapsedEventHandler(OnElapsedTimeAsync); 
+            upProdutos.Interval = 1* 1000;  
+            upProdutos.Enabled = true;
+            WriteDebug("Serviço iniciado, vs.:21082019");
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private  void OnElapsedTimeAsync(object source, ElapsedEventArgs e)
+        {
+            upProdutos.Stop();
+            if (timer.AddMinutes(Properties.Settings.Default.UpProdutos) <DateTime.Now)
+            {
+                timer = DateTime.Now;
+                WriteDebug("OnElapsedTimeAsync");               
+                up_variants();
+            }
+            upProdutos.Start();
+
+        }
+        private Int32 ProdutoCadastrado(Int32 codprod)
+        {
+            var client = new RestClient("https://eudireto.com/api/products?pcode="+codprod);
+            client.Authenticator = new HttpBasicAuthenticator("ruggeri.barbosa@viacerta.com.br", "hK8421we27khQ80P90H3T9918DMx347k");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            JObject produtosResponse = JObject.Parse(response.Content);
+            // WriteDegug("Response" + response.Content);
+
+            Int32 product_id =(Int32)produtosResponse["products"][0]["product_id"];
+            WriteDebug("Codigo do produto Winthor:"+codprod+ "\tproduct_id: "+ product_id);
+            return product_id;
+           // return produtosResponse["products"].Count().ToString() == "0"? false: true;
+
+        }
+
+        private  void up_variants()
+        {  
+            List<Products> lstProdutos = LstProdutos();
+            WriteDebug(JsonConvert.SerializeObject( lstProdutos,Formatting.Indented));
+            lstProdutos.ForEach(processarProduto);
+            ult_sinc_produtos = DateTime.Now;
+
+        }
+
+        private void processarProduto(Products produto)
+        { 
+            Int32 product_id = ProdutoCadastrado(produto.product_code);
+            WriteDebug("Subindo produto codigo_distribuidor:"+produto.product_code+ "(product_id: "+product_id+")");
+
+            var client = new RestClient("https://eudireto.com/api/products/" + product_id);
+            client.Authenticator = new HttpBasicAuthenticator("ruggeri.barbosa@viacerta.com.br", "hK8421we27khQ80P90H3T9918DMx347k");
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("Accept", "application/json");
+            WriteDebug(JsonConvert.SerializeObject(produto));
+            request.AddParameter("application/json", JsonConvert.SerializeObject(produto), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            WriteDebug(response.Content);
+
+           
+        }
+        
+
+        private List<Products> LstProdutos()
+        {
+            try { 
+            con.Open();
+            DataSet dataSet = new DataSet();
+
+            string query = @"
+                select
+                pcprodut.codprod, 
+                pcprodut.descricao,
+                case when pcprodut.codepto = 14 then 'Pilhas e Baterias' when  pcprodut.codepto = 64 then 'Energéticos' end categoria , 
+                TRUNC((pcest.qtestger - ( pcest.qtreserv + pcest.qtbloqueada)) ,0)  estoque ,
+                TRUNC(nvl( PCTABPR.pvenda,0),2)  PRECO,
+                PCPRODUT.embalagem,
+                pcprodut.nbm,
+                PCPRODUT.codauxiliar ean,
+                pcprodut.codauxiliar2 dun,
+                greatest(PCPRODUT.dtultalter, PCTABPR.dtultaltpvenda, PCEST.DTULTALTERSRVPRC) ULTIMA_MOVIMENTACAO 
+
+                from pcprodut, pcest , PCTABPR
+                where
+                PCPRODUT.obs2 <>'FL'
+                AND PCEST.codprod = PCPRODUT.CODPROD
+                AND PCPRODUT.codprod = PCTABPR.codprod
+                AND PCEST.codfilial  IN (:CODFILIAL)
+                AND PCPRODUT.CODEPTO IN(64,14) /*DEPARTAMENTOS ELEGÍVEIS PARA VENDAS  (12,14,40,70,71,55,42,64,63)*/
+                AND PCTABPR.numregiao IN(:REGIAO)
+           
+                AND greatest(PCPRODUT.dtultalter, PCTABPR.dtultaltpvenda, PCEST.DTULTALTERSRVPRC) >= to_date(:DTULTALT,'dd/mm/yyyy hh24:mi:ss')
+                 ";
+            OracleCommand fbcmd = new OracleCommand(query, con) { CommandType = CommandType.Text, BindByName = true };
+
+            fbcmd.Parameters.Add(":CODFILIAL", Properties.Settings.Default.codfilial);
+            fbcmd.Parameters.Add(":REGIAO", Properties.Settings.Default.RegiaoTblPreco);
+            fbcmd.Parameters.Add(":DTULTALT", ult_sinc_produtos.ToString("dd/MM/yyyy HH:mm:ss"));
+
+            OracleDataAdapter da = new OracleDataAdapter(fbcmd);
+            da.Fill(dataSet);
+            con.Close();
+            List<Products> itemsRows = new List<Products>();
+            for (int cont = 0; cont < dataSet.Tables[0].Rows.Count; cont++)
+            {
+                Int32 codprod =     Convert.ToInt32(dataSet.Tables[0].Rows[cont]["CODPROD"].ToString());
+                Int32  estoque =    Convert.ToInt32(dataSet.Tables[0].Rows[cont]["ESTOQUE"].ToString());
+                string descricao =  dataSet.Tables[0].Rows[cont]["DESCRICAO"].ToString();
+                string preco =      dataSet.Tables[0].Rows[cont]["PRECO"].ToString().Replace(",",".");
+                itemsRows.Add(new Products(codprod,descricao,estoque, Convert.ToDouble(preco)));
+            }
+            con.Close();
+            return itemsRows;
+            }
+            catch(Exception ex)
+            {
+                erroLogGeneration(ex.ToString());
+                if(con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                return null;
+            }
+        }
+
+        
+
+        public void WriteDebug(string texto)
+        {
+            if (Properties.Settings.Default.debugmode)
+            {
+                WriteToFile(DateTime.Now+"\t:"+texto);
+            }
+        }
+
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\Log_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+
+        public void erroLogGeneration(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ErroExceptions_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(DateTime.Now + "\t:" + Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(DateTime.Now + "\t:" + Message);
+                }
+            }
+        }
+                  
+
+        internal class Products
+        {   
+            public Products(Int32 codprod, string descricao, Int32 estoque, double preco)
+            {
+                product_code = codprod;
+                product = descricao;
+                amount = estoque;
+                price = preco;
+            }
+            public string product { get; set; }
+            public Int32 amount { get; set; }
+            public double price { get; set; }
+            public Int32 product_code { get; set; }
+
+        }
+
+    }
+}
